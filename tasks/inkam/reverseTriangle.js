b7991aeb2db59d4b281ef21a94e302d5832ab798
Raihaninkam <raihankamil37@gmail.com>
2025-07-13 05:54:37 +0700

    fix: add 'throw' to error handling module

--- tasks/inkam/reverseTriangle.js
+++ tasks/inkam/reverseTriangle.js
@@ -1,21 +1,16 @@
-function triangle2 (num) {
-    if (typeof num !== "number") {
-        console.log("Parameter harus bertipe data nomor dan harus nomor positif");
-    } 
+function triangle2(num) {
+    if (typeof num !== "number" || num <= 0) {
+        throw new Error("Parameter harus bertipe data number dan positif");
+    }
     let result = "";
-    for (let i = 1; i<= num; i++) {
+    for (let i = 1; i <= num; i++) {
         let row = "";
-            
-        for (let j = i; j >= 1; j-- ) {
+        for (let j = i; j >= 1; j--) {
             row += j + " ";
         }
-
-        if (i < num) {
-            result += row + "\n";
-        } else {
-            result += row;
-        }
+        result += (i < num) ? row + "\n" : row;
     }
     return result;
 }
 module.exports = triangle2;
+
