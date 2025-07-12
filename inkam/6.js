a02d5781c0820803affea074a914ef8a483fb987
Raihaninkam <raihankamil37@gmail.com>
2025-07-12 22:10:03 +0700

    fix: Delete folder inkam in root

--- inkam/6.js
+++
@@ -1,9 +0,0 @@
-function buatRentang (awal, akhir) {
-    let result = [];
-    for (let i = awal; i <= akhir; i++) {
-        result.push(i);
-
-    }
-    return result.join(", ");
-}
-module.exports = buatRentang;
