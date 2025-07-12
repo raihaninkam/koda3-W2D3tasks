f78a90ef0cbb5f7a44dc923eddc358af81d56c5e
Raihaninkam <raihankamil37@gmail.com>
2025-07-13 05:57:36 +0700

    fix: add 'throw' for error handling

--- tasks/inkam/range.js
+++ tasks/inkam/range.js
@@ -1,8 +1,10 @@
-function buatRentang (awal, akhir) {
+function buatRentang(awal, akhir) {
+    if (typeof awal !== "number" || typeof akhir !== "number") {
+        throw new Error("Parameter harus bertipe data number");
+    }
     let result = [];
     for (let i = awal; i <= akhir; i++) {
         result.push(i);
-
     }
     return result.join(", ");
 }
