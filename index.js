d96f4cc6276542704a4a70fc9b97af16f3d41437
Raihaninkam <raihankamil37@gmail.com>
2025-07-12 14:48:23 +0700

    feat: export function

--- index.js
+++ index.js
@@ -45,7 +45,91 @@ function tasksIrul(){
 tasksIrul();
 
 /**
- * Task "Person Name"
+ * Task Inkam
  */
 
-// import your tasks here
+const inkam = require("./tasks/inkam");
+
+function taskInkam() {
+    // Program: Segitiga
+    // Success
+    let segitiga = inkam.triangle(5);
+    console.log(segitiga);
+
+    // Failed
+    try {
+        const result = inkam.triangle("s");
+        console.log(result);
+    } catch(error) {
+        console.log(error.message);
+    }
+
+    // Program: countArray
+    // Success
+    const arr = [1,2,3,4,5];
+    const count = inkam.countArray(arr);
+    console.log(count);
+
+    // Failed
+    try {
+        const result = inkam.countArray("s");
+        console.log(result);
+    } catch(error) {
+        console.log(error.message);
+    }
+
+    // Program: Segitiga2
+    // Success
+    const segitiga2 = inkam.triangle2(5);
+    console.log(segitiga2);
+
+    // Failed
+    try {
+        const result = inkam.triangle2("s");
+        console.log(result);
+    } catch(error) {
+        console.log(error.message);
+    }
+
+    // Program: ProcessNumber
+    // Success
+    const number = [1,2,3,4,5];
+    console.log(inkam.processNumber(number));
+
+    // Failed
+    try {
+        const result = inkam.processNumber("s");
+        console.log(result);
+    } catch(error) {
+        console.log(error.message);
+    }
+
+    // Program: Cetak Tabel
+    // Success
+    const jumlah = 4;
+    console.log(inkam.cetakTabel(jumlah));
+
+    // Failed
+    try {
+        const result = inkam.cetakTabel("s");
+        console.log(result);
+    } catch(error) {
+        console.log(error.message);
+    }
+
+    // Program: Buat rentang
+    // Success
+    const rentang = inkam.buatRentang(1,10);
+    console.log(rentang);
+
+    // Failed
+    try {
+        const result = inkam.buatRentang("s");
+        console.log(result);
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+// Panggil fungsi
+taskInkam();
