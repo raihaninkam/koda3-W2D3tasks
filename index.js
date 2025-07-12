bdbfd58d624fa3bea6b4f465e5445e64c0396ef2
Raihaninkam <raihankamil37@gmail.com>
2025-07-12 22:36:57 +0700

    fix: fixed try catch handle on file index.js

--- index.js
+++ index.js
@@ -51,80 +51,62 @@ const inkam = require("./tasks/inkam");
 
 function taskInkam() {
     // Program: Segitiga
-    // Success
-    let segitiga = inkam.triangle(5);
-    console.log(segitiga);
-
-    // Failed
+    // Success   
     try {
-        const result = inkam.triangle("s");
-        console.log(result);
+        const segitiga = inkam.triangle(5);
+        console.log(segitiga);
+        // Failed    
     } catch(error) {
         console.log(error.message);
     }
 
     // Program: countArray
     // Success
-    const arr = [1,2,3,4,5];
-    const count = inkam.countArray(arr);
-    console.log(count);
-
-    // Failed
     try {
-        const result = inkam.countArray("s");
-        console.log(result);
+        const arr = [1,2,3,4,5];
+        const count = inkam.countArray(arr);
+        console.log(count);
+        // Failed    
     } catch(error) {
         console.log(error.message);
     }
 
     // Program: Segitiga2
     // Success
-    const segitiga2 = inkam.triangle2(5);
-    console.log(segitiga2);
-
-    // Failed
     try {
-        const result = inkam.triangle2("s");
-        console.log(result);
+        const segitiga2 = inkam.triangle2(5);
+        console.log(segitiga2);
+    // Failed
     } catch(error) {
         console.log(error.message);
     }
 
     // Program: ProcessNumber
     // Success
-    const number = [1,2,3,4,5];
-    console.log(inkam.processNumber(number));
-
-    // Failed
     try {
-        const result = inkam.processNumber("s");
-        console.log(result);
+        const number = [1,2,3,4,5];
+        console.log(inkam.processNumber(number));
+        // Failed
     } catch(error) {
         console.log(error.message);
     }
 
     // Program: Cetak Tabel
     // Success
-    const jumlah = 4;
-    console.log(inkam.cetakTabel(jumlah));
-
-    // Failed
     try {
-        const result = inkam.cetakTabel("s");
-        console.log(result);
+        const jumlah = 4;
+        console.log(inkam.cetakTabel(jumlah));
+    // Failed
     } catch(error) {
         console.log(error.message);
     }
 
     // Program: Buat rentang
     // Success
-    const rentang = inkam.buatRentang(1,10);
-    console.log(rentang);
-
-    // Failed
     try {
-        const result = inkam.buatRentang("s");
-        console.log(result);
+        const rentang = inkam.buatRentang(1,10);
+        console.log(rentang);
+        // Failed
     } catch(error) {
         console.log(error.message);
     }
