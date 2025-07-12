f62208298abc05579c1e12cb02baf934910d2253
Raihaninkam <raihankamil37@gmail.com>
2025-07-13 05:53:24 +0700

    fix: add 'throw' on error handling

--- tasks/inkam/countArray.js
+++ tasks/inkam/countArray.js
@@ -1,13 +1,11 @@
-function countArray (arr) {
+function countArray(arr) {
     if (!Array.isArray(arr)) {
-        console.log("Parameter harus berupa tipe data Array");
-    } else {
-        let result = 1;
-        for (let i = 0; i < arr.length; i++) {
-            result *= arr[i];
-            
-        }
-        return result;
+        throw new Error("Parameter harus berupa array");
     }
+    let result = 1;
+    for (let i = 0; i < arr.length; i++) {
+        result *= arr[i];
+    }
+    return result;
 }
 module.exports = countArray;
